841c461ce06f619ce4d32b5f0ee548c3d29bbdad
alltel908 <alltel@alltel.co.kr>
2025-07-14 16:54:54 +0900

    인덱스 파일 수정 최종

--- gpt/index.js
+++ gpt/index.js
@@ -1,13 +1,12 @@
 // gpt/index.js
 
-import { callGPTWithFAQ_before_esim } from './callGPTWithFAQ_before_esim.js';
-import { callGPTWithFAQ_before_usim } from './callGPTWithFAQ_before_usim.js';
-import { callGPTWithFAQ_before_wifi } from './callGPTWithFAQ_before_wifi.js';
-import { callGPTWithFAQ_after_esim } from './callGPTWithFAQ_after_esim.js';
-import { callGPTWithFAQ_after_usim } from './callGPTWithFAQ_after_usim.js';
-import { callGPTWithFAQ_after_wifi } from './callGPTWithFAQ_after_wifi.js';
+import callGPTWithFAQ_before_esim from './callGPTWithFAQ_before_esim.js';
+import callGPTWithFAQ_before_usim from './callGPTWithFAQ_before_usim.js';
+import callGPTWithFAQ_before_wifi from './callGPTWithFAQ_before_wifi.js';
+import callGPTWithFAQ_after_esim from './callGPTWithFAQ_after_esim.js';
+import callGPTWithFAQ_after_usim from './callGPTWithFAQ_after_usim.js';
+import callGPTWithFAQ_after_wifi from './callGPTWithFAQ_after_wifi.js';
 
-// ✅ 모든 전문가(GPT 함수)들을 하나의 목록으로 관리
 const gptFunctions = {
   before_esim: callGPTWithFAQ_before_esim,
   before_usim: callGPTWithFAQ_before_usim,
